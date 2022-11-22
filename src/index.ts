d361064c20ea1e8127812d0f6e2dccb610078336
Tomas Mehdi <tomymehdi@gmail.com>
2022-11-22 23:12:44 +0100

    created tests and did the code

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,47 @@
 import axios from 'axios';
 
-export function helloWorld() {
-  const message = 'Hello World from my example modern npm package!';
-  return message;
-}
-
-export function goodBye() {
-  const message = 'Goodbye from my example modern npm package!';
-  return message;
-}
-
-export async function externalRESTApi(): Promise<any> {
+export async function books(): Promise<any> {
   const result = await axios.get('https://the-one-api.dev/v2/book');
-  return result;
+  return result.data;
 }
 
-export async function externalRESTApi2(): Promise<any> {
-  const result = await axios.get('https://the-one-api.dev/v2/book/1');  
-  return result;
+export async function book(id: String): Promise<any> {
+  const result = await axios.get(`https://the-one-api.dev/v2/book/${id}`);
+  return result.data;
+}
+
+export async function bookChapters(id: String): Promise<any> {
+  const result = await axios.get(`https://the-one-api.dev/v2/book/${id}/chapter`);
+  return result.data;
+}
+
+export async function booksWithChapters(): Promise<any> {
+  const booksResult: any = await axios.get('https://the-one-api.dev/v2/book');
+  // console.log('booksResult.data: ', booksResult.data)
+
+  var booksChaptersPromises: any = [];
+  booksResult.data.docs.forEach( async (elem: any) => {
+    // console.log('elem._id: ', elem._id)
+    booksChaptersPromises.push(axios.get(`https://the-one-api.dev/v2/book/${elem._id}/chapter`));
+  })
+  // console.log('booksChaptersPromises: ', booksChaptersPromises)
+
+  const bookChapters: any = await Promise.all(booksChaptersPromises);
+  // console.log('bookChapters: ', bookChapters)
+
+  const resp = booksResult.data.docs.map( (elem: any, index: any) => {
+    return {
+      ...elem,
+      chapters: bookChapters[index].data.docs
+    }
+  })
+  // console.log('resp: ', resp)
+  return { docs: resp }
 }
 
 export default {
-  helloWorld,
-  goodBye,
-  externalRESTApi,
+  books,
+  book,
+  bookChapters,
+  booksWithChapters,
 };
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,13 +1,13 @@
 import 'mocha';
-import { assert } from 'chai';
-
+import { assert, expect } from 'chai';
+import path from 'path'
 // In case we need to mock external api calls
 import nock from 'nock'
 // In case we need to stub functions
-import sinon from 'sinon'
+// import sinon from 'sinon'
 
 
-import { helloWorld, goodBye, externalRESTApi, externalRESTApi2 } from '../src/index';
+import { books, book, bookChapters, booksWithChapters } from '../src/index';
 import npmPackage from '../src/index';
 
 describe('NPM Package', () => {
@@ -15,37 +15,24 @@ describe('NPM Package', () => {
     assert.isObject(npmPackage);
   });
 
-  it('should have a helloWorld property', () => {
-    assert.property(npmPackage, 'helloWorld');
+  it('should have a books property', () => {
+    assert.property(npmPackage, 'books');
+  });
+
+  it('should have a book property', () => {
+    assert.property(npmPackage, 'book');
+  });
+
+  it('should have a bookChapters property', () => {
+    assert.property(npmPackage, 'bookChapters');
+  });
+
+  it('should have a booksWithChapters property', () => {
+    assert.property(npmPackage, 'booksWithChapters');
   });
 });
 
-describe('Hello World Function', () => {
-  it('should be a function', () => {
-    assert.isFunction(helloWorld);
-  });
-
-  it('should return the hello world message', () => {
-    const expected = 'Hello World from my example modern npm package!';
-    const actual = helloWorld();
-    assert.equal(actual, expected);
-  });
-});
-
-describe('Goodbye Function', () => {
-  it('should be a function', () => {
-    assert.isFunction(goodBye);
-  });
-
-  it('should return the goodbye message', () => {
-    const expected = 'Goodbye from my example modern npm package!';
-    const actual = goodBye();
-    assert.equal(actual, expected);
-  });
-});
-
-import path from 'path'
-describe('External REST API Call', () => {
+describe('Books Function', () => {
   before(() => {
     nock.back.fixtures = path.join(`${__dirname}`, 'fixtures');
   });
@@ -64,26 +51,122 @@ describe('External REST API Call', () => {
   });
 
   it('should be a function', () => {
-    assert.isFunction(externalRESTApi);
+    assert.isFunction(books);
   });
 
-  it('should return an id', async () => {
-    const { nockDone } = await nock.back("search_for_project.json");
-    
-    const actual = await externalRESTApi();
-    assert.equal(actual.status, 200);
-    
-    nockDone();
-  });
+  it('should return a list of books', async () => {
+    const { nockDone } = await nock.back("books.json");
 
-  it('should return an doc', async () => {
-    const { nockDone } = await nock.back("search_for_project2.json");
-
-    const actual = await externalRESTApi2();
-    assert.equal(actual.status, 200);
+    const booksResponse = await books();
+    assert.equal(booksResponse.docs.length, 3);
+    expect(booksResponse.docs[0]).to.have.keys(['_id', 'name']);
+    expect(booksResponse.docs[0]).to.have.keys(['_id', 'name']);
+    expect(booksResponse.docs[0]).to.have.keys(['_id', 'name']);
 
     nockDone();
   });
+});
 
+describe('Book Function', () => {
+  before(() => {
+    nock.back.fixtures = path.join(`${__dirname}`, 'fixtures');
+  });
 
+  after(async () => {
+    nock.restore();
+  });
+
+  beforeEach(() => {
+    nock.back.setMode('record');
+  });
+
+  afterEach(async () => {
+    nock.back.setMode('wild');
+    nock.cleanAll();
+  });
+
+  it('should be a function', () => {
+    assert.isFunction(book);
+  });
+
+  it('should return a book', async () => {
+    const { nockDone } = await nock.back("book.json");
+
+    const bookId = '5cf5805fb53e011a64671582'
+    const bookResponse = await book(bookId);
+    expect(bookResponse.docs[0]).to.have.keys(['_id', 'name']);
+
+    nockDone();
+  });
+});
+
+describe('Book Chapters', () => {
+  before(() => {
+    nock.back.fixtures = path.join(`${__dirname}`, 'fixtures');
+  });
+
+  after(async () => {
+    nock.restore();
+  });
+
+  beforeEach(() => {
+    nock.back.setMode('record');
+  });
+
+  afterEach(async () => {
+    nock.back.setMode('wild');
+    nock.cleanAll();
+  });
+
+  it('should be a function', () => {
+    assert.isFunction(bookChapters);
+  });
+
+  it('should return the chapters of a book', async () => {
+    const { nockDone } = await nock.back("book_chapters.json");
+
+    const bookId = '5cf5805fb53e011a64671582'
+    const bookChaptersResponse = await bookChapters(bookId);
+    assert.equal(bookChaptersResponse.docs.length, 22);
+    expect(bookChaptersResponse.docs[0]).to.have.keys(['_id', 'chapterName']);
+    expect(bookChaptersResponse.docs[0]).to.have.keys(['_id', 'chapterName']);
+    expect(bookChaptersResponse.docs[0]).to.have.keys(['_id', 'chapterName']);
+
+    nockDone();
+  });
+});
+
+describe('Books with Chapters Function', () => {
+  before(() => {
+    nock.back.fixtures = path.join(`${__dirname}`, 'fixtures');
+  });
+
+  after(async () => {
+    nock.restore();
+  });
+
+  beforeEach(() => {
+    nock.back.setMode('record');
+  });
+
+  afterEach(async () => {
+    nock.back.setMode('wild');
+    nock.cleanAll();
+  });
+
+  it('should be a function', () => {
+    assert.isFunction(booksWithChapters);
+  });
+
+  it('should return a list of books with its chapters', async () => {
+    const { nockDone } = await nock.back("books_with_chapters.json");
+
+    const booksWithChaptersResponse = await booksWithChapters();
+    assert.equal(booksWithChaptersResponse.docs.length, 3);
+    expect(booksWithChaptersResponse.docs[0]).to.have.keys(['_id', 'name', 'chapters']);
+    expect(booksWithChaptersResponse.docs[0]).to.have.keys(['_id', 'name', 'chapters']);
+    expect(booksWithChaptersResponse.docs[0]).to.have.keys(['_id', 'name', 'chapters']);
+
+    nockDone();
+  });
 });
